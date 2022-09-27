42e2b7f7e63be26b94c73d5f31bd855a40830586
hunggtham <hunggtham@gmail.com>
2022-09-27 11:36:07 +0900

    update

---
+++ src/main/java/com/nipa/abandon/controller/UserController.java
@@ -0,0 +1,77 @@
+//package com.nipa.abandon.controller;
+//
+//import com.nipa.abandon.model.UserVo;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//
+//@Controller
+//@RequiredArgsConstructor
+//public class UserController {
+//
+//    private final UserService userService;
+//
+//    /**
+//     * localhost:8080 시 login 으로 redirect
+//     * @return
+//     */
+//    @GetMapping
+//    public String root() {
+//        return "redirect:/login";
+//    }
+//
+//    /**
+//     * 로그인 폼
+//     * @return
+//     */
+//    @GetMapping("/login")
+//    public String login(){
+//        return "login";
+//    }
+//
+//    /**
+//     * 회원가입 폼
+//     * @return
+//     */
+//    @GetMapping("/signUp")
+//    public String signUpForm() {
+//        return "signUp";
+//    }
+//
+//    /**
+//     * 로그인 실패 폼
+//     * @return
+//     */
+//    @GetMapping("/access_denied")
+//    public String accessDenied() {
+//        return "access_denied";
+//    }
+//
+//    /**
+//     * 회원가입 진행
+//     * @param userVo
+//     * @return
+//     */
+//    @PostMapping("/signUp")
+//    public String signUp(UserVo userVo) {
+//        userService.joinUser(userVo);
+//        return "redirect:/login";
+//    }
+//
+//    /**
+//     * 유저 페이지
+//     * @param model
+//     * @param authentication
+//     * @return
+//     */
+//    @GetMapping("/user_access")
+//    public String userAccess(Model model, Authentication authentication) {
+//        //Authentication 객체를 통해 유저 정보를 가져올 수 있다.
+//        UserVo userVo = (UserVo) authentication.getPrincipal();  //userDetail 객체를 가져옴
+//        model.addAttribute("info", userVo.getUserId() +"의 "+ userVo.getUserName()+ "님");      //유저 아이디
+//        return "user_access";
+//    }
+//}
