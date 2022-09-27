b38cfa1cddd3d78f2cf01a2aca110634a79a42af
hunggtham <hunggtham@gmail.com>
2022-09-27 12:10:02 +0900

    update

--- src/main/java/com/nipa/abandon/config/WebSecurityConfig.java
+++ src/main/java/com/nipa/abandon/config/WebSecurityConfig.java
@@ -1,51 +1,51 @@
-package com.nipa.abandon.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((requests) -> requests
-                        .antMatchers("/", "/home","/donate", "/about" , "/causes", "/contact" , "/donate", "/error", "/service", "/team", "testimonial","/login", "/singUp", "/access_denied", "/resources/**").permitAll()
-                        .anyRequest().authenticated()
-                        .antMatchers("/userAccess").hasRole("USER")
-                        .antMatchers("/userAccess").hasRole("ADMIN")
-                )
-                .formLogin()
-                .loginPage("/login")
-                .loginProcessingUrl("/login_proc")
-                .defaultSuccessUrl("/user_access")
-                .failureUrl("/access_denied") // 인증에 실패했을 때 보여주는 화면 url, 로그인 form으로 파라미터값 error=true로 보낸다.
-                .and()
-                .csrf().disable();		//로그인 창
-
-        return http.build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
-    }
-
-}
+//package com.nipa.abandon.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests((requests) -> requests
+//                        .antMatchers("/", "/home","/donate", "/about" , "/causes", "/contact" , "/donate", "/error", "/service", "/team", "testimonial","/login", "/singUp", "/access_denied", "/resources/**").permitAll()
+//                        .anyRequest().authenticated()
+//                        .antMatchers("/userAccess").hasRole("USER")
+//                        .antMatchers("/userAccess").hasRole("ADMIN")
+//                )
+//                .formLogin()
+//                .loginPage("/login")
+//                .loginProcessingUrl("/login_proc")
+//                .defaultSuccessUrl("/user_access")
+//                .failureUrl("/access_denied") // 인증에 실패했을 때 보여주는 화면 url, 로그인 form으로 파라미터값 error=true로 보낸다.
+//                .and()
+//                .csrf().disable();		//로그인 창
+//
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails user =
+//                User.withDefaultPasswordEncoder()
+//                        .username("user")
+//                        .password("password")
+//                        .roles("USER")
+//                        .build();
+//
+//        return new InMemoryUserDetailsManager(user);
+//    }
+//
+//}
